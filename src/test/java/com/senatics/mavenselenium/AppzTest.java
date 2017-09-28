e1adb3247c7d33fc2571d0813e368dc8e99eb942
aramirez <tonifer627@gmail.com>
2017-09-28 16:19:36 -0400

    Merge origin/master into HEAD
    Conflicts:
    	cobertura/reports/cobertura-report/coverage.xml

--- src/test/java/com/senatics/mavenselenium/AppzTest.java
+++ src/test/java/com/senatics/mavenselenium/AppzTest.java
@@ -29,7 +29,7 @@ public class AppzTest {
   @Before
   public void setUp() throws Exception {
     //System.setProperty("webdriver.chrome.driver", "mavenproject1/drivers/chromedriver.exe");
-    System.setProperty("webdriver.chrome.driver", "$/usr/local/bin/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "../../drivers/chromedriver");
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); 
     baseUrl = "https://www.senatics.gov.py/";
