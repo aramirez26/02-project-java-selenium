986b0eae9a63425679653b3b304b12dc54176e51
aramirez <tonifer627@gmail.com>
2017-10-17 12:57:15 -0300

    Merge origin/master into HEAD
    Conflicts:
    	cobertura/reports/cobertura-report/coverage.xml

--- src/test/java/com/senatics/mavenselenium/AppzTest.java
+++ src/test/java/com/senatics/mavenselenium/AppzTest.java
@@ -20,10 +20,10 @@ public class AppzTest {
   public void setUp() throws Exception {
     //System.setProperty("webdriver.gecko.driver", "mavenproject1/drivers/geckodriver.exe"); 
     //System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-    DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-    capabilities.setCapability("marionette", true);
-    WebDriver driver = new FirefoxDriver(capabilities);
-    //driver = new FirefoxDriver();
+////    DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+////    capabilities.setCapability("marionette", true);
+////    WebDriver driver = new FirefoxDriver(capabilities);
+    driver = new FirefoxDriver();
     baseUrl = "http://www.chortitzer.com.py/";
     //XvfbRunMojo.ROLE.startsWith(baseUrl);
     driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
